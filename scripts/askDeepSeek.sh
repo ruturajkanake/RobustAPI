98ecc71263d0b800fa8358781822f1fdb398eceb
Ruturaj Kanake <ruturajkanake1999@gmail.com>
2025-11-17 01:10:58 -0800

    Add script to run askHF with specified parameters

---
+++ scripts/askDeepSeek.sh
@@ -0,0 +1,8 @@
+GPU=$1
+
+python askHF.py \
+--ckpt_path deepseek-ai/deepseek-coder-1.3b-instruct \
+--tokenizer_path deepseek-ai/deepseek-coder-1.3b-instruct \
+--result_dir ./results/deepseek-coder-1.3b/ \
+--shot_number 1 \
+--gpu $GPU
