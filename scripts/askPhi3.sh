82f508fc5e6953712df7750b3e746288e4015110
Ruturaj Kanake <ruturajkanake1999@gmail.com>
2025-11-17 10:11:01 -0800

    Update askPhi3.sh to use Microsoft Phi-3 model for inference

--- scripts/askPhi3.sh
+++ scripts/askPhi3.sh
@@ -1,8 +1,8 @@
 GPU=$1
 
 python askHF.py \
---ckpt_path google/codegemma-2b \
---tokenizer_path google/codegemma-2b \
---result_dir ./results/codegemma-2b/ \
+--ckpt_path microsoft/Phi-3-mini-4k-instruct \
+--tokenizer_path microsoft/Phi-3-mini-4k-instruct \
+--result_dir ./results/phi3-mini-4k/ \
 --shot_number 1 \
 --gpu $GPU
