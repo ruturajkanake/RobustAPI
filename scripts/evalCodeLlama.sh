25dd252aa5e9f796ca3e5dfbad388dd8bada7649
Ruturaj Kanake <ruturajkanake1999@gmail.com>
2025-12-01 12:33:00 -0800

    Update evalCodeGemma.sh to use DeepSeek model and add evalCodeLlama.sh for Llama model evaluation

--- scripts/evalCodeLlama.sh
+++ scripts/evalCodeLlama.sh
@@ -1,8 +1,8 @@
 python eval/evaluator.py \
---answer_dir=./results/codegemma-2b/ \
+--answer_dir=./results// \
 --pattern_path=./eval/pat_list.txt \
 --dataset_path=./dataset/question.jsonl \
---model=deepseek \
+--model=llama \
 --passk=1
 
 rm .tmp*
